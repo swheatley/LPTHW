0f1f5c47e6f0ee8835e86bd1d25ee6dc9b87e83f
swheatley <shaylee1@me.com>
2015-08-22 14:13:07 -0600

    Exercises 12 through 14

---
+++ ex11.1.py
@@ -0,0 +1,12 @@
+# Exercise 11.1 -  Asking questions
+
+print "Are you male or female?"
+gender = raw_input()
+
+print "Are you a web designer or web developer?"
+programmer = raw_input()
+
+print "What language do you 'code' in?"
+language = raw_input()
+
+print "So you are %r, %r, and like coding in %r" % (gender,programmer, language)
