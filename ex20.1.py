a3f63cf183a4d1c44bb9c95dc4da4e1081e83886
swheatley <shaylee1@me.com>
2015-08-29 14:42:24 -0600

    Fixing indenting issues and variable errors on lesson 21

--- ex20.1.py
+++ ex20.1.py
@@ -1,26 +1,23 @@
-#Exercise 20: Functions and Files
-# run they file using test.txt in addition to python ex20.py ( like normal )
-
 from sys import argv
 
 script, input_file = argv
 
 def print_all(f):
-	print f.read()
+    print f.read()
 
 def rewind(f):
-	f.seek(0)
+    f.seek(0)
 
 def print_a_line(line_count, f):
-	print line_count, f.readline()
+    print line_count, f.readline()
 
 current_file = open(input_file)
 
-print "First let's print the whole file: \n"
+print "First let's print the whole file:\n"
 
 print_all(current_file)
 
-print "Now let's rewind, kind of like a tape"
+print "Now let's rewind, kind of like a tape."
 
 rewind(current_file)
 
--- ex21.py
+++ ex21.py
@@ -15,17 +15,17 @@ def divide(a,b):
 	print "DIVIDING %d / %d" % (a, b)
 	return a/b
 
-	print "Let's do some math with just functions!"
+print "Let's do some math with just functions!"
 
-	age = add(30, 5)
-	height = subtract(78, 4)
-	weight = mutliply (90, 2)
-	iq = divide(100, 2)
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply (90, 2)
+iq = divide(100, 2)
 
-	print "Age: %d, Height: %d, Weight: %d, IQ: %d" %(age, height, weight, iq)
-	# A puzzle for extra credit, type it in anyway.
-	print "Here is a puzzle."
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" %(age, height, weight, iq)
+# A puzzle for extra credit, type it in anyway.
+print "Here is a puzzle."
 
-	what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
-	print "That becomes: ", what, "Can you do it by hand?"
+print "That becomes: ", what, "Can you do it by hand?"
