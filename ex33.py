9c89060914494d56b43521330e12f5b542d1770d
swheatley <shaylee1@me.com>
2015-08-30 20:16:21 -0600

    Initial run through of Exercise 33 is complete

--- ex33.py
+++ ex33.py
@@ -2,3 +2,30 @@
 
 # While-loop rus until the expression is False
 
+# Rules: 
+	# Use while-loop spariling.  For-loop is better
+	# Make sure boolean test will become False at some point OR you will have an infinite loop
+
+
+i = 0
+numbers  = []
+
+while i < 6:
+	print "At the top i is %d" % i
+	numbers.append(i)
+
+	i = i + 1
+	print "Numbers now: ", numbers
+	print "At the bottom i is %d" % i
+
+print "The numbers: "
+
+for num in numbers:
+	print num
+
+
+
+
+
+
+
