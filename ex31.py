ccbab9536f075949fa45df4aad728326d0ffac55
swheatley <shaylee1@me.com>
2015-08-30 19:07:09 -0600

    Airbender word game completed.  Added all four elements to the game

--- ex31.py
+++ ex31.py
@@ -1,8 +1,8 @@
 # Exercise 31: Making Decisions
 
-print "Your name is Ang and you are the Avatar.  What bending skill would you like to learn first? Earth, Fire, Wind or Water?"
+print "You are Ang the last Airbender, What bending skill would you like to learn first? Earth, Fire, Air or Water?"
 
-beding = raw_input(">")
+bending = raw_input(">")
 
 if bending  == "Earth":
 	print "You need to find a master earth bender.  Who do you seek?."
@@ -19,26 +19,33 @@ if bending  == "Earth":
 		print "You take Apa and fly to the Air temple. You need some inspiration." 
 
 elif bending == "Fire":
-	print "You need to learn to firebend before the fire lord takes over during the eclipse. Who do you seek?"
+	print "You need to learn to firebend before the Fire Lord takes over. Who do you seek?"
 	print "1. Lord Zuko from Fire Nation."
 	print "2. Iroh, Uncle to Lord Zuko."
 
 	fire_bender = raw_input(">")
 
-	if fire_bender == "1" or fire_bender == "2":
-		print "Your body survives powered by a mind of jello. Good job!"
-	else:
-		print "The insanity rots your eyes into a pool of muck. Good job!"
+	if fire_bender == "1":
+		print "Lord Zuko helps you harness the power of fire.  You even meet real dragons!"
+	elif fire_bender == "2":
+		print "Iroh shows you how to redirect lighting.  This a skill only few know.  Then you sit down and enjoy some tea."
 
-elif bending == "Wind":
-	print "You are in a secret tunnel with strange sounds and smells....What do you do?"
-	print "1. Venture deep into the tunnel."
-	print "2."
+elif bending == "Air":
+	print "You were a monk at an Air temple and are a master Air bender. Training complete :)"
 	
 
-elif bending == "Water"
-	print ""
-	print ""
+elif bending == "Water":
+	print "You travel in the future and meet up with Korra of the Southern Water Tribe ."
+	print "You take Katara up on her water bending skills and practice your water wips!"
+
+	water_bender = raw_input(">")
+
+	if water_bender == "1":
+		print "She helps you master the art of water bending and you help her reach new heights in air bending!."
+	elif water_bender == "2":
+		print "You pick up water bending very quickly only after a few attempts.  Water bending is pretty easy for you!"
+	else:
+		print "You take Apa and fly to the Air temple. You need some inspiration."
 
 else:
 	print "You don't defeat the fire Lord and hope in the world is lost....."
