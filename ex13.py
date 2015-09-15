9b888e16f96d078f4cc1366f2070cdf24fd896be
swheatley <shaylee1@me.com>
2015-09-15 15:13:33 -0600

    Combined raw_input with argv

--- ex13.py
+++ ex13.py
@@ -2,9 +2,23 @@
 # Remember to include three variables before you run it
 from sys import argv
 
-script, first, second, third = argv
+argv =  raw_input("Give me a movie name: ")
+movie = argv
 
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is", second
-print "Your third variable is:", third
+argv =  raw_input("What is your favorite color?: ")
+color = argv
+
+argv = raw_input("Are you a cat or dog person?: ")
+animal = argv
+
+argv = int(raw_input("How old are you?: "))
+age = argv
+
+
+
+
+
+print "The movie is:", movie
+print "Favorite color:", color
+print "You are a %s person" % animal
+print "You are %d years old" % age
