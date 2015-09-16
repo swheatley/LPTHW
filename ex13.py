fa79d960665717abd15e66cd88835194779cb367
swheatley <shaylee1@me.com>
2015-09-15 22:52:40 -0600

    Deleted if/elif/else statment to movie question

--- ex13.py
+++ ex13.py
@@ -9,13 +9,6 @@ prompt = " ---> "
 print " %s what is your favorite Judd Apatow movie?" % userName
 movie = raw_input(prompt)
 
-if movie == "this is 40" or "knocked up":
-	print "It's classic!!!"
-elif movie == "the neighbors":
-	print "Growing up man!!!"
-else:
-	print "Life is about growing and learning"
-
 print " %s are you a cat or a dog person?" % userName
 animal = raw_input(prompt)
 
@@ -33,8 +26,10 @@ birthday = raw_input(prompt)
 print "Here is your story.."
 
 print """
+		%s life isn't like '%s'.  You are a grown %s who was born in %s.
+		Remember, your %s loves you.
 
-      """ % ()
+      """ % (userName, movie, gender, birthday, animal)
 
 
 
