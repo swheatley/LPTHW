bd02166fdd62c01029e963cdb8e37d3ebcf20140
swheatley <shaylee1@me.com>
2015-09-15 22:40:29 -0600

    Add if/elif/else statment to favorite J.A. movies

--- ex13.py
+++ ex13.py
@@ -6,8 +6,15 @@ script,userName = argv
 
 prompt = " ---> "
 
-print " %s what is your favorite movie?" % userName
+print " %s what is your favorite Judd Apatow movie?" % userName
 movie = raw_input(prompt)
+
+if movie == "this is 40" or "knocked up":
+	print "It's classic!!!"
+elif movie == "the neighbors":
+	print "Growing up man!!!"
+else:
+	print "Life is about growing and learning"
 
 print " %s are you a cat or a dog person?" % userName
 animal = raw_input(prompt)
@@ -24,6 +31,10 @@ print "What month is your birthday in?"
 birthday = raw_input(prompt)
 
 print "Here is your story.."
+
+print """
+
+      """ % ()
 
 
 
